afce6538d076082f69bbf1257a0669b56b46f7ad
amspokrm577 <72411574+amspokrm577@users.noreply.github.com>
2023-05-01 14:03:59 -0400

    Updated max_clique_approx.py

--- approximate_solution/test_cases/max_clique_approx.py
+++ approximate_solution/test_cases/max_clique_approx.py
@@ -3,29 +3,28 @@ Approximation for max clique
 
 Author: Ross Amspoker
 
-"""
+Problem type: Greedy
 
 """
 
-"""
+
 def greedy(graph, val):
     s = set()
-    s.add(val)
-    for u in graph[val]: # O(deg(v))
+    s.add(str(val))
+    for u in graph[val]:
         del graph[u]
     del graph[val]
-    while graph: # O(n)
-        curr = min(graph, key=lambda k: len(graph[k])) # O(n)
-        for u in graph[curr]: # O(deg(curr))
+    while graph:
+        curr = min(graph, key=lambda k: len(graph[k]))
+        for u in graph[curr]: 
             if u in graph: 
                 del graph[u]
         del graph[curr]
-        s.add(curr)
+        s.add(str(curr))
     return s
 
 
 def main():
-    # example usage:
     n = int(input())
     adj_list = {}
     for _ in range(n):
@@ -38,21 +37,14 @@ def main():
         adj_list[arr[0]].add(arr[1])
 
     verts = adj_list.keys()
-    complement = {i:{j for j in verts if j not in adj_list[i] and j != i} for i in verts} # O(v^2)
-    arr = sorted(complement, key=lambda k: len(complement[k])) 
-    # print("Upper bound:", len(adj_list[max(adj_list, key=lambda k: len(adj_list[k]))]))
+    complement = {i:{j for j in verts if j not in adj_list[i] and j != i} for i in verts}
+    arr = sorted(complement, key=lambda k: len(complement[k]))
     curr_m_clique = set()
-    for a in range(len(arr) // 4): # O(v / 4)
-        curr = greedy(complement.copy(), arr[a]) # O(v^2)
+    for a in range(len(arr) // 4):
+        curr = greedy(complement.copy(), arr[a])
         curr_m_clique = max(curr_m_clique, curr, key=len)
-    for c in curr_m_clique:
-        for j in curr_m_clique:
-            if c != j and j not in adj_list[c]:
-                print("error", c, adj_list[c], j)
-    print("Approximate max clique:", curr_m_clique)
+    print(" ".join(curr_m_clique))
     
 
 if __name__ == "__main__":
     main()
-
-
