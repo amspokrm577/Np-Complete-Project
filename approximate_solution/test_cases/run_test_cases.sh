a0c642287b6a3b3d7a553d409acf237a5751afbf
amspokrm577 <72411574+amspokrm577@users.noreply.github.com>
2023-05-01 14:19:39 -0400

    Update run_test_cases.sh

--- approximate_solution/test_cases/run_test_cases.sh
+++ approximate_solution/test_cases/run_test_cases.sh
@@ -104,7 +104,7 @@ else
 fi
 
 echo "----------------"
-echo "TEST 5: "
+echo "TEST 6: "
 expectedoutput="1 2 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"
 echo -e $divider
 echo "Expected output: "
@@ -121,7 +121,7 @@ else
 fi
 
 echo "----------------"
-echo "TEST 5: "
+echo "TEST 7: "
 expectedoutput="0 1 2 3 4"
 echo -e $divider
 echo "Expected output: "
@@ -138,7 +138,7 @@ else
 fi
 
 echo "----------------"
-echo "TEST 5: "
+echo "TEST 8: "
 expectedoutput="0 1 2 3 4"
 echo -e $divider
 echo "Expected output: "
